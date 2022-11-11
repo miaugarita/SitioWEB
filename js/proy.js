e83b19ebb9aff03463fe956d5b749fd6af01fa55
Margarita Trejo <m4rgosss@gmail.com>
2022-11-10 22:18:38 -0700

    Login

---
+++ js/proy.js
@@ -0,0 +1,23 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.13.0/firebase-app.js";
+  import{getDatabase,onValue,ref,set,child,get,update,remove}  
+  from "https://www.gstatic.com/firebasejs/9.13.0/firebase-database.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAtyVaImQfS36IhQxa_1eNWm6_BYTJUM6k",
+    authDomain: "uvuweb-ffecc.firebaseapp.com",
+    databaseURL: "https://uvuweb-ffecc-default-rtdb.firebaseio.com",
+    projectId: "uvuweb-ffecc",
+    storageBucket: "uvuweb-ffecc.appspot.com",
+    messagingSenderId: "811905934331",
+    appId: "1:811905934331:web:ba075a200b737c21e9a15e",
+    measurementId: "G-V7VVKC2RDF"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+  const db = getDatabase();
